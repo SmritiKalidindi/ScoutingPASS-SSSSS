f1bc16344628b75ffcbe8b8ab6632d91296a2ff8
Smriti <sim@MacBook-Air-89.local>
2024-09-23 11:03:29 -0400

    am I a programming god?

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "0pdUvFuOjnqM1yObGcobWE3hnkcrUCvZ3nJfzFc7iEobFopFkZ5zWx7A7KBqDcmt";
 /**
  * Get list of teams in event
  *
