81a17e13fad5065d027e9be42d93e8a54f58a5e2
Alan An <alananofficial@gmail.com>
2025-03-13 18:04:11 -0400

    2025ncwak

--- 2025/CU_config.js
+++ 2025/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025nccat",
+      "defaultValue": "2025ncwak",
       "required": "true"
     },
     { "name": "Match Level",
